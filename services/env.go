e5e39a35fbe53ebd0a325c1342be1ce2245e01ec
hedgeho9X <1736355688@qq.com>
2025-03-27 11:00:37 +0800

    fix:修改识别的prompt（未验证)

--- services/env.go
+++ services/env.go
@@ -2,5 +2,5 @@ package services
 
 const (
 	DoubaoLite    = "ep-20250311120726-h7xml"
-	AnalyzePrompt = "识别试卷 ，给出题号，题号对应的知识点，以及对应的题目内容的json，格式为{\n\"question_id\" :\"\",\n\"key\":\"\",\n\"content\":\" \",\n}-,Json数组格式发送，只保留json内容"
+	AnalyzePrompt = "识别试卷内容，提取每道题目的题号、知识点和题目内容。返回格式要求：每道题目占一行，格式为：{'question_id':'', 'key':'','content':''。返回 JSON 格式。当题目为空时返回空json"
 )
